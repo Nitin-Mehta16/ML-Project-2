728e99a89e0779b194ad93bdc53f34547e69182d
Nitin-Mehta16 <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>
2025-03-11 15:37:00 +0530

    notebook + execption.py + logger.py

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,7 @@
 ## For Exception handeling 
 
 import sys # sys library will automatically have information about any execption happening 
+from src.logger import logging 
 
 def error_message_details(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -10,12 +11,20 @@ def error_message_details(error, error_detail:sys):
     
     return error_message
     
-class CustomExecption(Exception):
-    def __init__(self, error_message,error_detail:sys):
-        super().__init__(error_message)
-        self.error_message=error_message_details(error_message,error_detail)
-
+class CustomExecption(Exception):  ## CustomExecption--> child class  of parent class -->Python's built-in Exception class
+    def __init__(self, error_message,error_detail:sys):  #error_detail:sys --> error_detail = sys
+        super().__init__(error_message) #Calls super().__init__(error_message), which initializes the built-in Exception class. #The Exception class stores error_message in args
+        self.error_message=error_message_details(error_message,error_detail) #if exception clas store erro_message then why are be re defining it?? =>> By default, Python's built-in Exception class will only store the original error message (e.g., "Something went wrong"), But to enhance debugging, we redefine it.
     def __str__(self):
-        return self.error_message
+        return self.error_message #__str__ is a special method (dunder method) in Python that defines how an object should be represented as a human-readable string when you use print(object) or str(object).
+
+
+if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomExecption(e,sys)
+
         
 
--- src/logger.py
+++ src/logger.py
@@ -4,15 +4,19 @@ import os
 from datetime import datetime 
 
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs", LOG_FILE)
+LOG_FILE_name= f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(),"logs_folder") #os.path.join(...) → Joins all the parts into a complete file path, ensuring correct formatting across different operating systems.
 os.makedirs(logs_path, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE_name)
 
-logging.basicConfig(
+# We use logging.basicConfig() to: ✅ Save logs to a file instead of just printing to the console.
+# ✅ Define a clear log format for better debugging.
+# ✅ Set the log level (INFO, WARNING, ERROR, etc.).
+
+logging.basicConfig( #We use logging.basicConfig() to configure the logging system so that logs are written to a file with a specific format and log level.
     filename= LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+    level=logging.INFO, #Set the log level (INFO, WARNING, ERROR, etc.). Only logs with a level equal to or higher than this level will be saved.logging. INFO logs INFO, WARNING, ERROR, and CRITICAL messages.
 )
 
